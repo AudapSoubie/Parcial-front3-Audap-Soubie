ec2baac2664ca0c642187d28d91604adb207dd86
Cecilia <cecilia.audap@gmail.com>
2023-08-14 21:01:29 -0300

    preparacion

---
+++ src/Components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      Esta es mi card 
+    </div>
+  )
+}
+
+export default Card
---
+++ src/Components/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <div>
+        <label htmlFor=""> escribi algo</label>
+        <input type="text" />
+        <label htmlFor="">escribi otra cosa</label>
+        <input type="text" />
+      
+    </div>
+  )
+}
+
+export default Form
